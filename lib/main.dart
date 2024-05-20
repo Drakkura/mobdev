61b52ae7ded959a587d9cf027eb18de9f74406e1
Arzaq Ajradika <arzaqajradika1@gmail.com>
2024-05-20 23:10:08 +0700

    splashview fix

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,8 @@ import 'package:login_sahabat_mahasiswa/view/login.view.dart';
 import 'firebase_options.dart';
 import 'package:workmanager/workmanager.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:login_sahabat_mahasiswa/view/splash.view.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
 void callbackDispatcher() {
   Workmanager().executeTask((task, inputData) {
@@ -18,15 +20,21 @@ void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+
+  // Initialize Notification Service here
+  NotificationService.initialize();
+
   Workmanager().initialize(
       callbackDispatcher, // The top-level function, aka callbackDispatcher
       isInDebugMode: true // features to debug background tasks
       );
+
   Workmanager().registerPeriodicTask(
     "2",
     "checkTasks",
     frequency: Duration(minutes: 15), // defines the frequency of task execution
   );
+
   runApp(const App());
 }
 
@@ -37,19 +45,25 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: StreamBuilder<User?>(
-        stream: FirebaseAuth.instance.authStateChanges(),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.active) {
-            if (snapshot.data != null) {
-              return Dashboard(); // User is logged in
-            }
-            return LoginView(); // User is not logged in
+      home: SplashView(), // Start with the SplashView
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<User?>(
+      stream: FirebaseAuth.instance.authStateChanges(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.active) {
+          if (snapshot.data != null) {
+            return Dashboard();
           }
-          return Center(
-              child: CircularProgressIndicator()); // Waiting for connection
-        },
-      ),
+          return LoginView();
+        }
+        return Center(child: CircularProgressIndicator());
+      },
     );
   }
 }
--- lib/view/splash.view.dart
+++ lib/view/splash.view.dart
@@ -2,22 +2,38 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:login_sahabat_mahasiswa/main.dart';
 import 'package:login_sahabat_mahasiswa/utils/colors.dart';
 import 'package:login_sahabat_mahasiswa/view/login.view.dart';
 
-class SplashView extends StatelessWidget {
-  const SplashView({Key? key});
+class SplashView extends StatefulWidget {
+  @override
+  _SplashViewState createState() => _SplashViewState();
+}
+
+class _SplashViewState extends State<SplashView> {
+  @override
+  void initState() {
+    super.initState();
+    _navigateToHome();
+  }
+
+  _navigateToHome() async {
+    // Wait for a few seconds
+    await Future.delayed(Duration(seconds: 3), () {
+      Navigator.of(context)
+          .pushReplacement(MaterialPageRoute(builder: (_) => Home()));
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    Timer(const Duration(seconds: 2), () {
-      Get.to(LoginView());
-    });
     return Scaffold(
-      backgroundColor: GlobalColors.mainColor,
+      backgroundColor: GlobalColors
+          .mainColor, // Replace with your actual GlobalColors.mainColor
       body: Center(
         child: Image.asset(
-          'assets/images/LogoPanjang.png',
+          'assets/images/LogoPanjang.png', // Make sure the path is correct
           height: 150,
           width: 300,
         ),
