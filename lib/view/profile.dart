09d3dc3a6efbc13f83799f60099373f479888221
YogaElfaraby <100010908+YogaElfaraby@users.noreply.github.com>
2024-06-05 09:13:26 +0700

    Update profile.dart

--- lib/view/profile.dart
+++ lib/view/profile.dart
@@ -1,8 +1,26 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:login_sahabat_mahasiswa/settings/setting.dart';
 import 'package:login_sahabat_mahasiswa/utils/colors.dart';
+import 'package:login_sahabat_mahasiswa/view/calendar.dart';
+import 'package:login_sahabat_mahasiswa/view/dashboard.dart';
 import 'package:login_sahabat_mahasiswa/view/edit_profile_page.dart';
 import 'package:login_sahabat_mahasiswa/view/widget/bottom.navigationbar.dart';
+
+void main() {
+  runApp(ProfileApp());
+}
+
+class ProfileApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Profile App',
+      home: ProfilePage(),
+    );
+  }
+}
 
 class ProfilePage extends StatefulWidget {
   @override
@@ -13,6 +31,8 @@ class _ProfilePageState extends State<ProfilePage> {
   late String name = "";
   late String username = "";
   String profileImageUrl = "assets/images/logoaja.png";
+  final _db = FirebaseFirestore.instance;
+  final _auth = FirebaseAuth.instance;
   final Map<String, int> weeklyTaskCompletion = {
     'Senin': 2,
     'Selasa': 4,
@@ -22,6 +42,40 @@ class _ProfilePageState extends State<ProfilePage> {
     'Sabtu': 6,
     'Minggu': 3,
   };
+
+  @override
+  void initState() {
+    super.initState();
+    getUserDataFromFirestore();
+  }
+
+  String capitalize(String s) {
+    if (s.isEmpty) return s;
+    return s[0].toUpperCase() + s.substring(1);
+  }
+
+  String capitalizeName(String fullName) {
+    return fullName.split(' ').map((word) => capitalize(word)).join(' ');
+  }
+
+  Future<void> getUserDataFromFirestore() async {
+    try {
+      User? user = _auth.currentUser;
+      if (user != null) {
+        final snapshot = await _db.collection("users").doc(user.uid).get();
+
+        if (snapshot.exists) {
+          setState(() {
+            username = capitalize(snapshot.data()!['username']);
+            name = capitalizeName("${snapshot.data()!['firstName']} ${snapshot.data()!['lastName']}");
+            profileImageUrl = snapshot.data()!['profileImageUrl']; // Ensure the profile image URL is fetched
+          });
+        }
+      }
+    } catch (e) {
+      print("Error getting user data: $e");
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -71,52 +125,53 @@ class _ProfilePageState extends State<ProfilePage> {
                 ),
               ),
               const SizedBox(height: 20),
-              Stack(
-                children: [
-                  CircleAvatar(
-                    radius: 50,
-                    backgroundColor: const Color.fromARGB(255, 71, 76, 80),
-                    backgroundImage: profileImageUrl.isNotEmpty 
-                      ? NetworkImage(profileImageUrl) // Use NetworkImage for URL
-                      : AssetImage('assets/images/logoaja.png') as ImageProvider,
-                  ),
-                  Positioned(
-                    bottom: 0,
-                    right: 0,
-                    child: Container(
-                      decoration: const BoxDecoration(
-                        shape: BoxShape.circle,
-                        color: Colors.white,
-                      ),
-                      padding: const EdgeInsets.all(4),
-                      child: GestureDetector(
-                        onTap: () async {
-                          final result = await Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => EditProfilePage(
-                                name: name,
-                                username: username,
-                                profileImageUrl: profileImageUrl,
+              GestureDetector(
+                child: Stack(
+                  children: [
+                    CircleAvatar(
+                      radius: 50,
+                      backgroundColor: const Color.fromARGB(255, 71, 76, 80),
+                      backgroundImage: profileImageUrl.isNotEmpty 
+                        ? NetworkImage(profileImageUrl) // Use NetworkImage for URL
+                        : AssetImage('assets/images/logoaja.png') as ImageProvider,
+                    ),
+                    Positioned(
+                      bottom: 0,
+                      right: 0,
+                      child: Container(
+                        decoration: const BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: Colors.white,
+                        ),
+                        padding: const EdgeInsets.all(4),
+                        child: GestureDetector(
+                          onTap: () async {
+                            final result = await Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => EditProfilePage(
+                                  name: name,
+                                  username: username,
+                                  profileImageUrl: profileImageUrl,
+                                ),
                               ),
-                            ),
-                          );
+                            );
 
-                          if (result != null && result is Map<String, String>) {
-                            setState(() {
-                              name = result['name']!;
-                              username = result['username']!;
-                              profileImageUrl = result['profileImageUrl']!;
-                            });
-                          }
-                        },
-                        child: const Icon(Icons.edit),
+                            if (result != null && result is Map<String, String>) {
+                              setState(() {
+                                name = result['name']!;
+                                username = result['username']!;
+                                profileImageUrl = result['profileImageUrl']!;
+                              });
+                            }
+                          },
+                          child: const Icon(Icons.edit),
+                        ),
                       ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
-
               const SizedBox(height: 20),
               Row(
                 crossAxisAlignment: CrossAxisAlignment.start,
@@ -143,74 +198,67 @@ class _ProfilePageState extends State<ProfilePage> {
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
-                  // Grafik performa mingguan dalam menyelesaikan tugas
-                  Expanded(
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.pushReplacement(
+                        context,
+                        PageRouteBuilder(
+                          pageBuilder: (_, __, ___) => const Dashboard(),
+                          transitionDuration: Duration.zero,
+                        )
+                      );
+                    },
                     child: Container(
                       margin: const EdgeInsets.symmetric(vertical: 20),
                       padding: const EdgeInsets.all(20),
+                      width: MediaQuery.of(context).size.width / 2 - 10,
                       decoration: BoxDecoration(
                         color: Colors.grey[200],
                         borderRadius: BorderRadius.circular(10),
                       ),
-                      child: Column(
+                      child: const Column(
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
                           Text(
-                            'Weekly Task Completion',
+                            'Deadline Tugas!',
+                            style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold, color: Colors.red),
+                          ),
+                          Text(
+                            ' 2',
                             style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
                           ),
-                          SizedBox(height: 10),
-                          // Bar untuk setiap hari
-                          Container(
-                            height: 150,
-                            child: ListView(
-                              scrollDirection: Axis.horizontal,
-                              children: [
-                                // Sumbu Y
-                                Container(
-                                  padding: const EdgeInsets.only(right: 10),
-                                  child: Column(
-                                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                    children: [
-                                      for (int i = 10; i >= 0; i -= 2)
-                                        Container(
-                                          height: 15,
-                                          alignment: Alignment.centerRight,
-                                          child: Text(
-                                            '$i',
-                                            style: TextStyle(fontSize: 12),
-                                          ),
-                                        ),
-                                    ],
-                                  ),
-                                ),
-                                // Grafik Bar
-                                ...weeklyTaskCompletion.keys.map((day) {
-                                  return Flexible(
-                                    child: Column(
-                                      children: [
-                                        SizedBox(height: 5),
-                                        Expanded(
-                                          child: FractionallySizedBox(
-                                            heightFactor: weeklyTaskCompletion[day]! / 10,
-                                            child: Container(
-                                              width: 20,
-                                              color: Colors.blue,
-                                              alignment: Alignment.bottomCenter,
-                                            ),
-                                          ),
-                                        ),
-                                        SizedBox(height: 5),
-                                        Text(
-                                          day,
-                                          style: TextStyle(fontSize: 12),
-                                        ),
-                                      ],
-                                    ),
-                                  );
-                                }).toList(),
-                              ],
-                            ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.pushReplacement(
+                        context,
+                        PageRouteBuilder(
+                          pageBuilder: (_, __, ___) => const CalendarScreen(),
+                          transitionDuration: Duration.zero,
+                        )
+                      );
+                    },
+                    child: Container(
+                      margin: const EdgeInsets.symmetric(vertical: 20),
+                      padding: const EdgeInsets.all(20),
+                      width: MediaQuery.of(context).size.width / 2 - 50,
+                      decoration: BoxDecoration(
+                        color: Colors.grey[200],
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: const Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'Meeting: 8',
+                            style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                          ),
+                          Text(
+                            'Task: 3',
+                            style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
                           ),
                         ],
                       ),
@@ -218,10 +266,83 @@ class _ProfilePageState extends State<ProfilePage> {
                   ),
                 ],
               ),
+              const SizedBox(height: 5,),
+              Expanded(
+                child: Container(
+                  margin: const EdgeInsets.symmetric(vertical: 20),
+                  padding: const EdgeInsets.all(20),
+                  decoration: BoxDecoration(
+                    color: Colors.grey[200],
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Weekly Task Completion',
+                        style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                      ),
+                      SizedBox(height: 10),
+                      // Bar for each day
+                      Container(
+                        height: 150,
+                        child: ListView(
+                          scrollDirection: Axis.horizontal,
+                          children: [
+                            // Y-axis
+                            Container(
+                              padding: const EdgeInsets.only(right: 10),
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                children: [
+                                  for (int i = 10; i >= 0; i -= 2)
+                                    Container(
+                                      height: 15,
+                                      alignment: Alignment.centerRight,
+                                      child: Text(
+                                        '$i',
+                                        style: TextStyle(fontSize: 12),
+                                      ),
+                                    ),
+                                ],
+                              ),
+                            ),
+                            // Bar Graph
+                            ...weeklyTaskCompletion.keys.map((day) {
+                              return Flexible(
+                                child: Column(
+                                  children: [
+                                    SizedBox(height: 5),
+                                    Expanded(
+                                      child: FractionallySizedBox(
+                                        heightFactor: weeklyTaskCompletion[day]! / 10,
+                                        child: Container(
+                                          width: 20,
+                                          color: Colors.blue,
+                                          alignment: Alignment.bottomCenter,
+                                        ),
+                                      ),
+                                    ),
+                                    SizedBox(height: 5),
+                                    Text(
+                                      day,
+                                      style: TextStyle(fontSize: 12),
+                                    ),
+                                  ],
+                                ),
+                              );
+                            }).toList(),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
             ],
           ),
         ),
       ),
     );
   }
-}    
+}
